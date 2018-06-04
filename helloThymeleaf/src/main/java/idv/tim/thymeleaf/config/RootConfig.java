4239f1dd9cbe1f66dfb5b33aef675af35ccc7819
tcwangh <ctwang01@gmail.com>
2018-06-04 21:25:45 +0800

    wildfly test done

---
+++ helloThymeleaf/src/main/java/idv/tim/thymeleaf/config/RootConfig.java
@@ -0,0 +1,16 @@
+package idv.tim.thymeleaf.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.FilterType;
+@Configuration
+@ComponentScan(basePackages={"idv.tim.thymeleaf"}, 
+    excludeFilters={
+        @Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)
+    })
+public class RootConfig {
+  
+}
