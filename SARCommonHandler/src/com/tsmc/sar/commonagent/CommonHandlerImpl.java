13dfe3a78ebb4ff3524954308585047c6604eb74
tcwangh <tcwangh@LAPTOP-4N902OFT>
2020-04-25 13:19:46 +0800

    OK

---
+++ SARCommonHandler/src/com/tsmc/sar/commonagent/CommonHandlerImpl.java
@@ -0,0 +1,12 @@
+package com.tsmc.sar.commonagent;
+
+public class CommonHandlerImpl {
+	
+	public String manageEventHandlerRequest_Sync(String x0,String x1, String x2) {
+		return x0+x1+x2;
+	}
+	
+	public String manageTimerHandlerRequest_Sync(String x0) {
+		return x0;
+	}
+}
